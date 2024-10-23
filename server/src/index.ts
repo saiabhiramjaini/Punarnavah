f5e67f619c0189a8d5adb9e9fb7144662369aa15
saiabhiramjaini <abhiramjaini28@gmail.com>
2024-10-23 12:44:02 +0530

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,7 +18,9 @@ const app = express();
 
 app.use(cors(
   {
-    origin: process.env.CLIENT_URL,
+    origin: [process.env.CLIENT_URL!,
+      "https://punarnavah-server.abhiramverse.tech/"
+    ],
     credentials: true,
   }
 ));
